15636b9667a5ca1073543116ab5b6a0d1023e400
MilyRodz <milycrn26@gmail.com>
2021-03-14 00:30:01 +0000

    create booking component

---
+++ src/front/js/component/Booking.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Booking = () => (
+	<div className="footer mt-auto py-3 text-center">
+		<p>
+			Book an appointment
+			<a href="http://www.4geeksacademy.com">Home</a>
+		</p>
+	</div>
+);
