93c2a314a3f4107f4f4afad79cc2fed0746183e6
MilyRodz <milycrn26@gmail.com>
2021-03-27 14:01:45 +0000

    refactorizar componente join-us

--- src/front/js/component/JoinUsBanner.js
+++ src/front/js/component/JoinUsBanner.js
@@ -1,24 +1,14 @@
 import React from "react";
-import catBanner from "../../img/cat-banner.jpg";
 import { ButtonRegister } from "./ButtonRegister";
 
 const JoinUsBanner = () => {
 	return (
-		<div className="container-fluid">
-			<div className="card mt-3 border-0 w-100">
-				<div className="">
-					<div className="">
-						<img width={1400} height={500} src={catBanner} />
-					</div>
-					<div className="card-img-overlay">
-						<div className="card-body text-left">
-							<h5 className="card-title pl-4">Nos encantaria conecerte a ti y a tu mascota</h5>
-							<h5 className="card-title pl-4">
-								<ButtonRegister />
-							</h5>
-						</div>
-					</div>
-				</div>
+		<div className="jumbotron jumbotron-fluid jumbotron-join-us p-0">
+			<div className="container">
+				<h1 className="display-4">Nos encantaria conecerte a ti y a tu mascota</h1>
+				<h5 className="">
+					<ButtonRegister />
+				</h5>
 			</div>
 		</div>
 	);
