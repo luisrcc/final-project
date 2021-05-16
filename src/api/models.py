37d2562620324fa1e77ec3ff9cfd58242503926e
MilyRodz <milycrn26@gmail.com>
2021-05-16 15:34:13 +0000

    corrige lógica de modelo specialist

--- src/api/models.py
+++ src/api/models.py
@@ -14,13 +14,12 @@ class User(db.Model):
     first_name = db.Column(db.String(250))
     last_name = db.Column(db.String(250))
     email = db.Column(db.String(250), unique=True, nullable=False)
-    phone = db.Column(db.Integer)
-    # password = db.Column(db.String(250), nullable=False)
+    phone = db.Column(db.Integer)    
     password = db.Column(db.String(255))
     perfil_id = db.Column(db.Integer, db.ForeignKey('perfil.id'), nullable=False)
 
     appointment = db.relationship('Appointment', backref='user', lazy=True)
-
+    specialist_rel = db.relationship('Specialist', backref='user', lazy=True)
         
     def __repr__(self):
         return '<User %r>' % self.email
@@ -41,7 +40,7 @@ class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     speciality_id = db.Column(db.Integer, db.ForeignKey('speciality.id'), nullable=False)
-    specialist_id = db.Column(db.Integer, db.ForeignKey('specialist.id'), nullable=False)
+    specialist_id = db.Column(db.Integer, db.ForeignKey('specialist.user_id'), nullable=False)
     working_hour_id = db.Column(db.Integer, db.ForeignKey('working_hours.id'), nullable=False)
     pet_name = db.Column(db.String(250), unique=False, nullable=False)
     pet = db.Column(db.String(250), unique=False, nullable=False)
@@ -66,7 +65,7 @@ class Appointment(db.Model):
 class Working_hours(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True) 
     speciality_id = db.Column(db.Integer, db.ForeignKey('speciality.id'), nullable=False)
-    specialist_id = db.Column(db.Integer, db.ForeignKey('specialist.id'), nullable=False)
+    specialist_id = db.Column(db.Integer, db.ForeignKey('specialist.user_id'), nullable=False)
     time = db.Column(db.TIME, nullable=False)
 
     appointment_rel = db.relationship('Appointment', backref='working_hours', lazy=True)
@@ -78,7 +77,7 @@ class Working_hours(db.Model):
         return {
             "id": self.id,
             "speciality_id": self.speciality_id,
-            "specialist_id": self.specialist_id,
+            "specialist_id": self.user_id,
             "time": self.time.strftime("%H:%M:%S")
         }
 
@@ -88,7 +87,7 @@ class Speciality(db.Model):
     
     specialist = db.relationship('Specialist', backref='speciality', lazy=True)
     appointment = db.relationship('Appointment', backref='speciality', lazy=True)
-    specialist_rel = db.relationship('Working_hours', backref='speciality', lazy=True)
+    working_hours_rel = db.relationship('Working_hours', backref='speciality', lazy=True)
 
     def _repr_(self):
         return '<Speciality %r>' % self.name
@@ -99,9 +98,9 @@ class Speciality(db.Model):
             "name":self.name
         }
 
-class Specialist(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True)    
-    speciality_id = db.Column(db.Integer, db.ForeignKey('speciality.id'), nullable=False)
+class Specialist(db.Model):    
+    specialist_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)    
+    speciality_id = db.Column(db.Integer, db.ForeignKey("speciality.id"), primary_key=True)
     name = db.Column(db.String(250), unique=False, nullable=False)
 
     appointment_rel = db.relationship('Appointment', backref='specialist', lazy=True)
@@ -112,7 +111,7 @@ class Specialist(db.Model):
 
     def serialize(self):
         return {
-            "id": self.id,
+            "specialist_id": self.specialist_id,
             "name": self.name,
             "speciality_id": self.speciality_id
         }
--- src/api/services.py
+++ src/api/services.py
@@ -22,9 +22,8 @@ def get_times_by_speciality(id_speciality, id_specialist, date, user_id):
     now = datetime.now(timeZ_As).time()
     nowDate = datetime.now(timeZ_As)
 
-    
     newDateParam = datetime.strptime(date, '%Y-%m-%d')
-    
+
     if newDateParam.strftime('%Y-%m-%d') == nowDate.strftime('%Y-%m-%d'):
         now = datetime.now(timeZ_As).time()
     elif newDateParam.strftime('%Y-%m-%d') < nowDate.strftime('%Y-%m-%d'):
@@ -38,8 +37,7 @@ def get_times_by_speciality(id_speciality, id_specialist, date, user_id):
 
     ids_horas_tomadas = db.session.query(Appointment.working_hour_id).filter(
         Appointment.speciality_id == id_speciality, Appointment.specialist_id == id_specialist, Appointment.date == date, Appointment.user_id == user_id)
-    
-    
+
     response = db.session.query(Working_hours).filter(Working_hours.specialist_id == id_specialist,
                                                       Working_hours.speciality_id == id_speciality, ~Working_hours.id.in_(ids_horas_tomadas),  Working_hours.time >= now)
     if response:
@@ -57,6 +55,7 @@ def get_booking_list_client(speciality_id, specialist_id, user_id):
         Appointment.speciality_id == speciality_id, Appointment.specialist_id == specialist_id, Appointment.user_id == user_id)
     response = db.session.query(Working_hours).filter(Working_hours.specialist_id == specialist_id,
                                                       Working_hours.speciality_id == speciality_id, Working_hours.id.in_(ids_horas_tomadas),  Working_hours.time >= now)
+    
 
     if response:
         return response
